ae9f52b453d8124a4e4dd9a7854a699d27a032c4
Yohanes Abiti <abitiyohanis42@gamil.com>
2024-11-13 10:21:37 +0300

    Fix line endings in src/index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Header from "./componets/header/header"
+import Footer from "./componets/footer/footer"
 function App() {
   return (
     <div className="App">
-    <h1>hello everybody</h1>
+    <Header/>
+    <Footer/>
+
     </div>
   );
 }
